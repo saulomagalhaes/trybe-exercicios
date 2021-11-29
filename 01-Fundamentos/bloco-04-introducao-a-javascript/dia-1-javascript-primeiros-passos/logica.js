b891088ca0fd308c793ec9d236e962c9ecad6968
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2021-11-28 22:11:28 -0300

    Exercicio 4.3 - Dia 4.1

---
+++ 01-Fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/logica.js
@@ -0,0 +1,9 @@
+const nota = 79.0;
+
+if(nota >= 80){
+  console.log('Parabéns, você foi apropvado')
+} else if(nota < 80 && nota >= 60){
+  console.log('Você está na nossa lista de espera')
+} else{
+  console.log('Você foi reprovado')
+}
