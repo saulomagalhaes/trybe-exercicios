0bb9bba6894ee951bc8164b89d3e164bdca3e0e2
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2021-11-29 13:52:46 -0300

    Feito ate o exercicio 5

--- 01-Fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/logica.js
+++ 01-Fundamentos/bloco-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/logica.js
@@ -1,55 +1,56 @@
-// Exercicio prático 1
-// const a = 10;
-// const b = 5;
-// let adicao = a + b;
-// let subtracao = a - b;
-// let multiplicacao = a * b;
-// let divisao = a / b;
-// let modulo = a % b;
-// console.log(adicao)
-// console.log(subtracao)
-// console.log(multiplicacao)
-// console.log(divisao)
-// console.log(modulo)
-// -----------------------
+/* Exercicio prático 1
+const a = 10;
+const b = 5;
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+console.log(adicao)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(modulo)
+----------------------- */
 
-// Exercicio pratico 2
-// const n1 = 13;
-// const n2 = 6;
-// if(n1 > n2){
-//   console.log(`O número ${n1} é maior que o número ${n2}`)
-// } else if(n2 > n1){
-//   console.log(`O número ${n2} é maior que o número ${n1}`)
-// }else{
-//   console.log('Os números são iguais')
-// }
-// --------------------------
+/* Exercicio pratico 2
+const n1 = 13;
+const n2 = 6;
+if(n1 > n2){
+  console.log(`O número ${n1} é maior que o número ${n2}`)
+} else if(n2 > n1){
+  console.log(`O número ${n2} é maior que o número ${n1}`)
+}else{
+  console.log('Os números são iguais')
+}
+-------------------------- */
 
-// Exercício prático 3
-// const n1 = 10;
-// const n2 = 10;
-// const n3 = 10;
-// if(n1 > n2 && n1 > n3){
-//   console.log(`O número ${n1} é maior que o número ${n2} e maior que o número ${n3} `)
-// } else if(n2 > n1 && n2 > n3){
-//   console.log(`O número ${n2} é maior que o número ${n1} e maior que o número ${n3}`)
-// }else if(n3 > n1 && n3 > n2){
-// console.log(`O número ${n3} é maior que o número ${n1} e maior que o número ${n2}`)
-// } else {
-//   console.log('Os números são iguais')
-// }
+/* Exercício prático 3
+const n1 = 10;
+const n2 = 10;
+const n3 = 10;
+if(n1 > n2 && n1 > n3){
+  console.log(`O número ${n1} é maior que o número ${n2} e maior que o número ${n3} `)
+} else if(n2 > n1 && n2 > n3){
+  console.log(`O número ${n2} é maior que o número ${n1} e maior que o número ${n3}`)
+}else if(n3 > n1 && n3 > n2){
+console.log(`O número ${n3} é maior que o número ${n1} e maior que o número ${n2}`)
+} else {
+  console.log('Os números são iguais')
+}
+ */
 
-// Exercício prático 4
-// const numero = -1;
-// if(numero > 0){
-//   console.log('Positive')
-// }else if(numero < 0){
-//   console.log('Negative')
-// } else{
-//   console.log('Zero')
-// }
+/* Exercício prático 4
+const numero = -1;
+if(numero > 0){
+  console.log('Positive')
+}else if(numero < 0){
+  console.log('Negative')
+} else{
+  console.log('Zero')
+} */
 
-// Exercício prático 5
+/* Exercício prático 5
 const angulo1 = 60;
 const angulo2 = 60;
 const angulo3 = 60;
@@ -64,6 +65,6 @@ if (somaPositivos) {
   };
 } else {
   console.log('Angulo Invalido')
-}
+} */
 
 // Exercicio 6
