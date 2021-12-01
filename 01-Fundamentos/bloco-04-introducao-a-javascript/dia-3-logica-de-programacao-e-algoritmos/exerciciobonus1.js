f5c632d44bcff02fdb281e127f9670adc3a79539
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2021-12-01 10:54:26 -0300

    Exercicio bonus ate o 2

---
+++ 01-Fundamentos/bloco-04-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exerciciobonus1.js
@@ -0,0 +1,9 @@
+let n = 5
+let asterisco = '*'
+let linha = ''
+for (let index = 0; index < n; index += 1) {
+  linha += asterisco
+}
+for (let index = 0; index < n; index += 1) {
+  console.log(linha)
+}
---
+++ 01-Fundamentos/bloco-04-introducao-a-javascript/dia-3-logica-de-programacao-e-algoritmos/exerciciobonus2.js
@@ -0,0 +1,7 @@
+let n = 5
+let asterisco = '*'
+let linha = ''
+for (let index = 0; index < n; index += 1) {
+  linha += asterisco
+  console.log(linha)
+}
