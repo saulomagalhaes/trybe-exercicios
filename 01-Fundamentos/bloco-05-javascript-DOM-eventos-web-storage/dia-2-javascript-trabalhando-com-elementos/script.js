a5c311a2d114e884b546cd5ac2499a916fe7fea4
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2021-12-06 18:30:44 -0300

    Exemplos feitos

---
+++ 01-Fundamentos/bloco-05-javascript-DOM-eventos-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,53 @@
+// PARTE 1
+
+// const elementoOndeVoceEsta = document.getElementById("elementoOndeVoceEsta");
+// const pai = elementoOndeVoceEsta.parentElement
+
+// pai.style.color = "red"
+
+// const primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild
+// primeiroFilhoDoFilho.innerText = 'Saulo'
+
+// const primeirofilho = pai.firstElementChild
+
+// const primeirofilho2 = elementoOndeVoceEsta.previousElementSibling
+
+// const atencao = elementoOndeVoceEsta.nextSibling
+
+// const terceiroFilho= elementoOndeVoceEsta.nextElementSibling
+
+// const terceiroFIlho = pai.lastElementChild.previousElementSibling
+
+
+
+// PARTE 2
+
+// const pai = document.getElementById('pai')
+// const irmaoElementoOndeVoceEsta = document.createElement('section')
+// irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta'
+// pai.appendChild(irmaoElementoOndeVoceEsta)
+
+// const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta')
+// const filho = document.createElement('section')
+// filho.id = 'Filho'
+// elementoOndeVoceEsta.appendChild(filho)
+
+// const filho = document.getElementById('primeiroFilhoDoFilho')
+// const filhoDoFilho = document.createElement('div')
+// filhoDoFilho.id = 'filhoDofilho'
+// filho.appendChild(filhoDoFilho)
+// const terceiroFilho = filhoDoFilho.parentElement.parentElement.nextElementSibling
+// console.log(terceiroFilho)
+
+
+// PARTE 3
+// const pai = document.getElementById('pai')
+// for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+//   const filhoAtual = pai.childNodes[index]
+//   if (filhoAtual.id !== 'elementoOndeVoceEsta') {
+//     filhoAtual.remove()
+//   }
+// }
+
+
+
