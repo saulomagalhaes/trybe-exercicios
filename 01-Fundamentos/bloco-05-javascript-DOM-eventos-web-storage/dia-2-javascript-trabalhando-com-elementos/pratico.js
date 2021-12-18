1108f2268ae2d1b489799ddcc583e2f243cfa79f
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2021-12-18 15:21:57 -0300

    Todos os exercicios do dia 5.2 refeitos

--- 01-Fundamentos/bloco-05-javascript-DOM-eventos-web-storage/dia-2-javascript-trabalhando-com-elementos/pratico.js
+++ 01-Fundamentos/bloco-05-javascript-DOM-eventos-web-storage/dia-2-javascript-trabalhando-com-elementos/pratico.js
@@ -6,6 +6,7 @@ const p = document.createElement('p');
 const sectionLeft = document.createElement('section');
 const sectionRight = document.createElement('section');
 const img = document.createElement('img');
+const ul = document.createElement('ul');
 
 //REQ.1
 function createH1() {
@@ -58,3 +59,61 @@ function createImage() {
 createImage();
 
 //REQ.8
+function createList() {
+  let array = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+  for (let i of array) {
+    let createLi = document.createElement('li');
+    createLi.innerText = i;
+    ul.appendChild(createLi);
+  }
+  sectionRight.appendChild(ul)
+}
+createList();
+
+//REQ.9
+function createH3() {
+  for (let i = 0; i < 3; i += 1) {
+    const h3 = document.createElement('h3');
+    main.appendChild(h3);
+  }
+}
+createH3();
+
+// PARTE2
+
+//REQ.1
+function createClassTitle() {
+  h1.classList.add('title')
+}
+createClassTitle();
+
+// REQ.2
+function createClassH3() {
+  const allH3 = document.getElementsByTagName('h3');
+  for (let i of allH3) {
+    i.classList.add('description');
+  }
+}
+createClassH3();
+
+//REQ.3
+function removeSectionLeft() {
+  main.removeChild(sectionLeft);
+}
+removeSectionLeft();
+
+// REQ.4 --->> NO CSS
+
+// REQ.5
+function changeColorMain() {
+  main.style.background = 'green';
+}
+changeColorMain();
+
+//REQ.6
+function removeItensUl() {
+  let children = ul.children;
+  ul.removeChild(children[9]);
+  ul.removeChild(children[8]);
+}
+removeItensUl()
