35bb043077298e652d1ff557db7ab1dd8f949e5e
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-01-15 22:19:20 -0300

    Parte 3

--- 01-Fundamentos/bloco-07-JavaScript-ES6/dia-03-jest/functions.js
+++ 01-Fundamentos/bloco-07-JavaScript-ES6/dia-03-jest/functions.js
@@ -16,4 +16,14 @@ const myRemove = (arr, item) => {
   return newArr;
 };
 
-module.exports = { sum, myRemove };
+// eslint-disable-next-line complexity
+const myFizzBuzz = (num) => {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+};
+
+// implemente seus testes aqui
+module.exports = { sum, myRemove, myFizzBuzz };
--- 01-Fundamentos/bloco-07-JavaScript-ES6/dia-03-jest/functions.test.js
+++ 01-Fundamentos/bloco-07-JavaScript-ES6/dia-03-jest/functions.test.js
@@ -1,5 +1,6 @@
-const { sum, myRemove } = require('./functions');
+const { sum, myRemove, myFizzBuzz } = require('./functions');
 
+/* PARTE 1 */
 describe('Função de somar', () => {
   it('Teste se 4 + 5 = 9', () => {
     expect(9).toBe(sum(4, 5));
@@ -17,6 +18,7 @@ describe('Função de somar', () => {
   });
 });
 
+/* PARTE 2 */
 describe('Remover item de um array', () => {
   it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado ', () => {
     expect([1, 2, 4]).toEqual(myRemove([1, 2, 3, 4], 3));
@@ -26,5 +28,28 @@ describe('Remover item de um array', () => {
   });
   it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado ', () => {
     expect([1, 2, 3, 4]).toEqual(myRemove([1, 2, 3, 4], 5));
+  });
+});
+
+/* PARTE 3 */
+// eslint-disable-next-line max-len
+describe('- A função myFizzBuzznum recebe um número num e retorna "fizzbuzz" se o número for divisível por 3 e 5 , retorna "fizz" se for divisível apenas por 3 , retorna "buzz" se divisível apenas por 5 , retorna o próprio número caso não seja divisível por 3 ou 5 e retorna false caso num não seja um número', () => {
+  // eslint-disable-next-line max-len
+  it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect('fizzbuzz').toBe(myFizzBuzz(15));
+  });
+  it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+    expect('fizz').toBe(myFizzBuzz(9));
+  });
+  it('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+    expect('buzz').toBe(myFizzBuzz(5));
+  });
+  // eslint-disable-next-line max-len
+  it('Faça uma chamada com um número que não é divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(7)).toBe(myFizzBuzz(7));
+  });
+  // eslint-disable-next-line max-len
+  it('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+    expect(false).toBe(myFizzBuzz('5'));
   });
 });
