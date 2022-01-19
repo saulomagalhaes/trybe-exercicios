89b1034cdb2ee3f58ec69f4f0cf44ac556ad4d44
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-01-19 17:04:45 -0300

    Sort.js

---
+++ 01-Fundamentos/bloco-08-higer-order-functions/dia-02-HOF/someevery.js
@@ -0,0 +1,21 @@
+//1 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some ;
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) =>
+  arr.some((item) => (item === name ? true : false));
+
+console.log(hasName(names, 'Saulo'));
+
+//2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every ;
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) =>
+  arr.every((item) => (item.age >= minimumAge ? true : false));
+
+console.log(verifyAges(people, 18));
