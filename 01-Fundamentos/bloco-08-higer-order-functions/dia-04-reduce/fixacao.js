fe5f103e95f4aea54a2ee94b154dca47881c2576
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-01-21 15:28:22 -0300

    Exercicio 1 e 2

---
+++ 01-Fundamentos/bloco-08-higer-order-functions/dia-04-reduce/fixacao.js
@@ -0,0 +1,100 @@
+// FILTER E REDUCE
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const sumPairs = () =>
+  numbers.filter((n) => n % 2 === 0).reduce((acc, n) => acc + n);
+
+console.log(sumPairs());
+
+//APENAS REDUCE
+const action = (acc, n) => (n % 2 === 0 ? acc + n : acc);
+
+const sumPairsReduce = () => numbers.reduce(action, 0);
+
+console.log(sumPairsReduce());
+
+//MAP E REDUCE
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const decrease = (acc, n) => (acc.nota > n.nota ? acc : n);
+
+const bestMateria = () =>
+  estudantes.map((n) => ({
+    name: n.nome,
+    matéria: n.materias.reduce(decrease).name,
+  }));
+
+console.log(bestMateria());
