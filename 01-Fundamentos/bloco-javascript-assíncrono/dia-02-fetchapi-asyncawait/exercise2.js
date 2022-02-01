3d72b84d2f703339b6d4a98c8c0f493ecbd2a8fe
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-02-01 10:32:56 -0300

    Exercicio finalizado

--- 01-Fundamentos/bloco-javascript-assíncrono/dia-02-fetchapi-asyncawait/exercise2.js
+++ 01-Fundamentos/bloco-javascript-assíncrono/dia-02-fetchapi-asyncawait/exercise2.js
@@ -1,15 +1,28 @@
-// api.js
-const fetch = require('node-fetch');
+// PRA RODAR O FETCH NO PROPRIO NODE, SE FOR RODAR NO NAVEGADOR NAO PRECISA
+// const fetch = require('node-fetch');
 
 const fetchCoins = async () => {
-  const url = 'https://api.coincap.io/v2/assets';
+  try {
+    const url = 'https://api.coincap.io/v2/assets'; /* URL DA API */
+    const response = await fetch(url); /* PEGA TODOS OS DADOS DA API */
+    const data = await response.json();/* RETORNA O JSON DOS DADOS O DATA */
+    const arrayData = data.data;/* RETORNA O ARRAY COM TODAS AS CRIPTOS */
+    return arrayData
+  } catch (err) {
+    return err.toString();
+  }
 
-  const coins = await fetch(url)
-    .then((response) => response.json())
-    .then((data) => data.data)
-    .catch((error) => error.toString());
+};
 
-  console.log(coins);
-}
-
-fetchCoins();
+const generateList = async () => {
+  const coins = await fetchCoins();
+  const ul = document.getElementById('coins-list');
+  coins.filter((coin)=>Number(coin.rank) <=10)
+  .forEach((coin) => {
+    const li = document.createElement('li');
+    const twoHouseCoins = Number(coin.priceUsd);
+    li.innerText = `${coin.name} (${coin.symbol}): $${twoHouseCoins.toFixed(2)} Dólares`
+    ul.appendChild(li);
+  })
+} 
+generateList()
