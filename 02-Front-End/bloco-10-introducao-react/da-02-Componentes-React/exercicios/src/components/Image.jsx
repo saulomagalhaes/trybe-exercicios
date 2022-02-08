e8505668170566f44653d202cea7ed8c2b49e435
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-02-08 17:09:22 -0300

    Fixacao 3

--- 02-Front-End/bloco-10-introducao-react/da-02-Componentes-React/exercicios/src/components/Image.jsx
+++ 02-Front-End/bloco-10-introducao-react/da-02-Componentes-React/exercicios/src/components/Image.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Image extends React.Component {
   render() {
-    return <img src={this.props.parameters.source} alt={this.props.parameters.alternativeText} />;
+    return <img src={this.props.source} alt={this.props.alternativeText} />;
   }
 }
 
