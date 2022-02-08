0686c2bb20eb662065d2391068f15ae564173da0
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-02-08 15:00:02 -0300

    Fixaxao 2

--- 02-Front-End/bloco-10-introducao-react/da-02-Componentes-React/exercicios/src/App.jsx
+++ 02-Front-End/bloco-10-introducao-react/da-02-Componentes-React/exercicios/src/App.jsx
@@ -1,16 +1,37 @@
+// arquivo App.js, criado pelo create-react-app, modificado
 import React from 'react';
-import Image from './components/Image';
+import Order from './components/Order';
 
-const props = {
-  source: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
-  alternativeText: 'Texto Alternativo',
-};
-function App() {
-  return (
-    <main>
-      <Image parameters={props} />
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: 'cena@gmail.com',
+      product: 'Razer Headphone',
+      price: {
+        value: 99.99,
+        currency: 'dollars',
+      },
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: 'cena@gmail.com',
+      product: 'Monster 500mL',
+      price: {
+        value: 9.99,
+        currency: 'dollars',
+      },
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order order={headphone} />
+        <Order order={energyDrink} />
+      </div>
+    );
+  }
 }
 
 export default App;
