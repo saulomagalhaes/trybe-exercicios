9fa851cf5e4edd11c9fc5d283b556a12ebc9beee
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-06-07 20:00:59 -0300

    feat(exercicios): exercicio 4.2

--- 03-Back-End/Bloco-22-NodeJs/dia-02-fluxo-assincrono/simpsons.js
+++ 03-Back-End/Bloco-22-NodeJs/dia-02-fluxo-assincrono/simpsons.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 
 const simpsonsJson = './simpsons.json';
 
-const simpsons = async () => {
+/* const simpsons = async () => {
   try {
     const resp = await fs.readFile(simpsonsJson, 'utf8');
     const data = JSON.parse(resp);
@@ -13,16 +13,32 @@ const simpsons = async () => {
   }
 };
 
-simpsons();
+simpsons(); */
 
-/*  const simpsons = () => {
-  return fs
-    .readFile(simpsonsJson, 'utf8')
-    .then((data) => {
-      const json = JSON.parse(data);
-      return console.log(json);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}; */
+//  const simpsons = () => {
+//   return fs
+//     .readFile(simpsonsJson, 'utf8')
+//     .then((data) => {
+//       const json = JSON.parse(data);
+//       return console.log(json);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+const searchForId = async (id) => {
+  try {
+    const resp = await fs.readFile(simpsonsJson, 'utf8');
+    const data = JSON.parse(resp);
+    const found = data.find((item) => Number(item.id) === id);
+    if (!found) {
+      throw new Error('Não encontrado');
+    }
+    return console.log(found);
+  } catch (err) {
+    return console.log(err);
+  }
+};
+
+searchForId(1);
