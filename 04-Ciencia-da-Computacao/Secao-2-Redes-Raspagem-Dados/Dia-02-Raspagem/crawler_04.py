a3f10f58ed03d921e56c07a64fac46f24ab4fbaa
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-11-05 21:02:43 -0300

    feat: :sparkles: adiciona quantidade de itens em estoque

--- 04-Ciencia-da-Computacao/Secao-2-Redes-Raspagem-Dados/Dia-02-Raspagem/crawler_04.py
+++ 04-Ciencia-da-Computacao/Secao-2-Redes-Raspagem-Dados/Dia-02-Raspagem/crawler_04.py
@@ -11,6 +11,7 @@ price = selector.css(".price_color::text").get()
 price_without_symbol = price.replace("Â£", "")
 description = selector.css("#product_description + p::text").get()
 img_url = selector.css(".item.active img::attr(src)").get()
+stock = selector.css(".product_main .availability::text").re_first("\d")
 
 
-print(f"{title}, {price_without_symbol}, {description}, {img_url}")
+print(f"{title}, {price_without_symbol}, {description}, {img_url}, {stock}")
